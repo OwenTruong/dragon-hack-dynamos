8280c985cee1dbf2aaefbae62c2d3ad231c3f348
OwenTruong <44269025+OwenTruong@users.noreply.github.com>
2023-04-30 00:57:29 -0400

    add global interface

--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -1,1 +1,37 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference types="react-scripts" />
+
+import { string } from 'zod';
+
+interface Pet {
+  petId: number;
+  name: string;
+  description: string;
+  adoptionProcess: string;
+  species: string;
+  breed: string | undefined;
+  age: number;
+  petImageUrl: string;
+  qrUrl: string | undefined;
+  createdAt: Date;
+  updatedAt: Date;
+  adoptCenterId: number;
+}
+
+interface AdoptionCenter {
+  adoptCenterId: number;
+  email: string;
+  password: string;
+  phone: string;
+  websiteURL: string | undefined;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface Adopter {
+  adopterId: number;
+  email: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
